64180361c39d66a894c861187656c61b530b2d01
supersourabh <70961689+supersourabh@users.noreply.github.com>
2021-08-03 11:10:47 +0530

    Update ViewScreen.js

--- frontend/src/Screens/ViewScreen.js
+++ frontend/src/Screens/ViewScreen.js
@@ -36,7 +36,7 @@ export default function ViewScreen(props) {
                 loading?<Loading/>:
                 error?<Alerts info="danger" message={" Not Found !!!"}/>:
                 item&&
-                <embed title="fisdfjoufsh" src={`data:${item.doc.contentType};base64,${imageSrc(item)}`} type={item.doc.contentType} width="100%" height="100%" ></embed>
+                <embed src={`data:${item.doc.contentType};base64,${imageSrc(item)}`} type={item.doc.contentType} width="100%" height="100%" />
             }
 
         </div>
